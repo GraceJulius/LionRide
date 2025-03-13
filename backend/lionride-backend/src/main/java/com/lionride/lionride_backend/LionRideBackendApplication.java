f5271be648f40cef26ae43465ce33301e898367e
Kachi <emmakeo91@gmail.com>
2025-03-12 20:19:17 -0400

    feat: Initialize Spring Boot backend and update configuration files

---
+++ backend/lionride-backend/src/main/java/com/lionride/lionride_backend/LionRideBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lionride.lionride_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionRideBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LionRideBackendApplication.class, args);
+	}
+
+}
