28e9a335078ff79f7b2e6a427e7123f656542fc1
GraceJulius <juliusgrace65@gmail.com>
2025-03-19 09:52:16 -0400

    deleted files

---
+++ frontend/lionride-frontend/src/components/ui/Navbar.js
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+      <nav>
+        <a href="app">Home</a>
+        <a href="/auth/login">Login</a>
+        <a href="/auth/register">Register</a>
+        <a href="/dashboard">Dashboard</a>
+      </nav>
+    );
+  }
+  
